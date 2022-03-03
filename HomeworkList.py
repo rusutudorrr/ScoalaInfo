9d60d000b62e5abcbb052f38fd0b9d91cdc283e6
Tudor Rusu <tudormrusu@yahoo.co.uk>
2022-03-03 12:02:56 +0200

    Add my homework to the repository

---
+++ HomeworkList.py
@@ -0,0 +1,33 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+
+print(my_list)
+
+
+my_listcopy = my_list.copy()
+
+my_listcopy.sort()
+
+print(my_listcopy)
+
+
+my_listcopy.reverse()
+
+print(my_listcopy)
+
+
+my_listcopy.reverse()
+
+
+my_odd_list = my_listcopy [1::2]
+
+print(my_odd_list)
+
+
+my_even_list = my_listcopy [0::2]
+
+print(my_even_list)
+
+
+my_multiples3 = my_listcopy[2::3]
+
+print(my_multiples3)
